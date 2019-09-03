6c7ebd5ccf762da8805fc59cd824c3a4fee18ff4
Mark <markzero27@gmail.com>
2019-09-04 01:28:50 +0800

    Add menu controller

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string;
-  constructor() {}
+  constructor(private menuController: MenuController) {}
 
   ngOnInit() {}
+
+  onClick() {
+    this.menuController.open();
+  }
 }
