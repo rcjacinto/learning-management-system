41558eee03f3775b813851648cc22fbf2adee022
Mark <markzero27@gmail.com>
2019-09-05 23:37:57 +0800

    Add students UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: 'Dashboard',
+      url: '/tabs/dashboard',
+      icon: 'school'
     }
   ];
 
