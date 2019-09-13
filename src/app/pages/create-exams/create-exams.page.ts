c23c168739ae4e2332f6d02e9cb2d4fcedaf12e2
Mark <markzero27@gmail.com>
2019-09-13 09:45:39 +0800

    Initialize create exam UI

--- src/app/pages/create-exams/create-exams.page.ts
+++ src/app/pages/create-exams/create-exams.page.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-exams',
   templateUrl: './create-exams.page.html',
-  styleUrls: ['./create-exams.page.scss'],
+  styleUrls: ['./create-exams.page.scss']
 })
 export class CreateExamsPage implements OnInit {
+  segment = 'details';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  segmentChanged(event) {
+    this.segment = event.target.value;
   }
-
 }
