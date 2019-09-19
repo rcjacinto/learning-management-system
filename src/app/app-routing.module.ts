19b33ca4157f44e665d62f05bd29ea0e213284b5
Mark <markzero27@gmail.com>
2019-09-19 14:53:24 +0800

    Generate login/register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,9 @@ const routes: Routes = [
   { path: 'library', loadChildren: './pages/library/library.module#LibraryPageModule' },
   { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsPageModule' },
   { path: 'records', loadChildren: './pages/records/records.module#RecordsPageModule' },
-  { path: 'create-exams', loadChildren: './pages/create-exams/create-exams.module#CreateExamsPageModule' }
+  { path: 'create-exams', loadChildren: './pages/create-exams/create-exams.module#CreateExamsPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' }
 ];
 
 @NgModule({
