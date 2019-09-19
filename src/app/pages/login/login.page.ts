f5c95449720fd2a097d1fd83d78ba4bbf7b94d6b
Mark <markzero27@gmail.com>
2019-09-19 20:37:51 +0800

    Add login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  loginIsClicked = false;
+  submitted = false;
+  loading = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private navController: NavController
+  ) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
 
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onLoginClick() {
+    this.loginIsClicked = true;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService
+      .doLogin(this.registerForm.value)
+      .then(res => {
+        console.log(res);
+        this.navController.navigateRoot('/tabs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
