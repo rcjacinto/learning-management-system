5f5a6bcc1cd9c18b7a282cf513717660d4436735
Mark <markzero27@gmail.com>
2019-09-23 17:24:17 +0800

    Initialize register UI

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  styleUrls: ['./register.page.scss']
 })
 export class RegisterPage implements OnInit {
+  submitted = false;
+  registerForm: FormGroup;
+  registering = false;
+  userType = 'instructor';
+  loading = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      classcode: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      password2: ['', [Validators.required]]
+    });
   }
 
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  selectType(type) {
+    this.userType = type;
+    this.registering = true;
+    if (type === 'instructor') {
+      this.registerForm.get('classcode').setValue('code');
+    } else {
+      this.registerForm.get('classcode').setValue('');
+    }
+  }
+
+  cancelRegistering() {
+    this.registering = false;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log(this.registerForm.value.name);
+  }
 }
