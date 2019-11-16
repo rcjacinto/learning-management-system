fab5459cf063ac0652f173d055b4d83565494da0
Mark <markzero27@gmail.com>
2019-11-16 10:42:38 +0800

    Fix student list

--- src/app/pages/students/students.page.ts
+++ src/app/pages/students/students.page.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { RootState, selectUser } from 'src/app/store';
-import { Class } from 'src/app/models/class.model';
-import { ClassService } from 'src/app/services/class.service';
-import { UserService } from 'src/app/services/user.service';
-import { ModalController } from '@ionic/angular';
-import { ViewProfileComponent } from 'src/app/components/view-profile/view-profile.component';
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { RootState, selectUser } from "src/app/store";
+import { Class } from "src/app/models/class.model";
+import { ClassService } from "src/app/services/class.service";
+import { UserService } from "src/app/services/user.service";
+import { ModalController } from "@ionic/angular";
+import { ViewProfileComponent } from "src/app/components/view-profile/view-profile.component";
 
 @Component({
-  selector: 'app-students',
-  templateUrl: './students.page.html',
-  styleUrls: ['./students.page.scss']
+  selector: "app-students",
+  templateUrl: "./students.page.html",
+  styleUrls: ["./students.page.scss"]
 })
 export class StudentsPage implements OnInit {
   userData$ = this.store.pipe(select(selectUser));
@@ -18,19 +18,24 @@ export class StudentsPage implements OnInit {
   classlist: Class[] = [];
   selectedClass: Class;
   members = [];
-  search = '';
+  search = "";
 
   constructor(
     private store: Store<RootState>,
     private classService: ClassService,
     private userService: UserService,
     public modalController: ModalController
-  ) {}
+  ) {
+    this.userData$.subscribe(user => {
+      this.user = user;
+    });
+  }
 
   async setSelectedClass() {
     console.log(this.selectedClass.members);
 
     if (this.selectedClass.members) {
+      this.members = [];
       this.selectedClass.members.forEach(member => {
         this.userService.getUser(member).subscribe(student => {
           this.members.push(student);
@@ -42,21 +47,14 @@ export class StudentsPage implements OnInit {
   ngOnInit() {}
 
   ionViewWillEnter() {
-    this.userData$.subscribe(user => {
-      this.user = user;
-      if (user) {
-        this.classService.getAllclasses(user.id).subscribe(async list => {
-          this.classlist = list;
-          if (this.classlist[0]) {
-            this.selectedClass = this.classlist[0];
-            console.log(this.selectedClass);
-            if (this.selectedClass.members) {
-              this.setSelectedClass();
-            }
-          }
-        });
-      }
-    });
+    if (this.user) {
+      this.classService.getAllclasses(this.user.id).subscribe(async list => {
+        this.classlist = list;
+        if (this.classlist[0]) {
+          this.selectedClass = this.classlist[0];
+        }
+      });
+    }
   }
 
   async viewProfile(user) {
